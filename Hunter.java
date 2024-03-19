c67a5cb003bd8cf349723f5f6b605bbaee7b22ec
Natasha Fasanotti <nfasanotti@spartaglobal.com>
2024-03-19 15:11:59 +0000

    Added haircolour to Hunter contructor

--- Hunter.java
+++ Hunter.java
@@ -2,9 +2,10 @@ package com.spartaglobal.clf;
 
 public class Hunter extends Person implements IShootable {
     private IShootable shootable;
-    public Hunter(String fName, String lName, int age, IShootable shootable) {
+    public Hunter(String fName, String lName, int age, IShootable shootable, String haircolour) {
         super(fName, lName, age);
         this.shootable = shootable;
+        this.haircolour = haircolour;
     }
     public String shoot() {
         return getFullName() + " is " + shootable.shoot() + "!!!";
